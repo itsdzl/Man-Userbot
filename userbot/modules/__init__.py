08ba4f62ab32ee964bd963959397d0576aa9176e
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-01-07 17:40:32 +0700

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -43,3 +43,44 @@ def __list_all_modules():
 ALL_MODULES = sorted(__list_all_modules())
 LOGS.info("Modules To Load : %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
+
+def vc_asst(dec, **kwargs):
+    def ult(func):
+        kwargs["func"] = (
+            lambda e: not e.is_private and not e.via_bot_id and not e.fwd_from
+        )
+        handler = udB.get_key("VC_HNDLR") or HNDLR
+        kwargs["pattern"] = re.compile(f"\\{handler}" + dec)
+        vc_auth = kwargs.get("vc_auth", True)
+        key = udB.get_key("VC_AUTH_GROUPS") or {}
+        if "vc_auth" in kwargs:
+            del kwargs["vc_auth"]
+
+        async def vc_handler(e):
+            VCAUTH = list(key.keys())
+            if not (
+                (e.out)
+                or (e.sender_id in VC_AUTHS())
+                or (vc_auth and e.chat_id in VCAUTH)
+            ):
+                return
+            elif vc_auth and key.get(e.chat_id):
+                cha, adm = key.get(e.chat_id), key[e.chat_id]["admins"]
+                if adm and not (await admin_check(e)):
+                    return
+            try:
+                await func(e)
+            except Exception:
+                LOGS.exception(Exception)
+                await asst.send_message(
+                    LOG_CHANNEL,
+                    f"VC Error - <code>{UltVer}</code>\n\n<code>{e.text}</code>\n\n<code>{format_exc()}</code>",
+                    parse_mode="html",
+                )
+
+        vcClient.add_event_handler(
+            vc_handler,
+            events.NewMessage(**kwargs),
+        )
+
+    return ult
