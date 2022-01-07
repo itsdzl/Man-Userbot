07231ca70b41d813643b9e1b4d08e8db2fd30763
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-01-07 18:08:08 +0700

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -102,3 +102,125 @@ async def vc_joiner(self):
             parse_mode="html",
         )
         return False
+
+class Player:
+    def __init__(self, chat, event=None, video=False):
+        self._chat = chat
+        self._current_chat = event.chat_id if event else LOG_CHANNEL
+        self._video = video
+        if CLIENTS.get(chat):
+            self.group_call = CLIENTS[chat]
+        else:
+            _client = GroupCallFactory(
+                vcClient, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON,
+                path_to_log_file="VCBot.log"
+            )
+            self.group_call = _client.get_group_call()
+            CLIENTS.update({chat: self.group_call})
+
+    async def make_vc_active(self):
+        try:
+            await vcClient(
+                functions.phone.CreateGroupCallRequest(
+                    self._chat, title="🎧 Ultroid Music 🎶"
+                )
+            )
+        except Exception as e:
+            LOGS.exception(e)
+            return False, e
+        return True, None
+
+    async def startCall(self):
+        if VIDEO_ON:
+            for chats in VIDEO_ON:
+                await VIDEO_ON[chats].stop()
+            VIDEO_ON.clear()
+            await asyncio.sleep(3)
+        if self._video:
+            for chats in list(CLIENTS):
+                if chats != self._chat:
+                    await CLIENTS[chats].stop()
+                    del CLIENTS[chats]
+            VIDEO_ON.update({self._chat: self.group_call})
+        if self._chat not in ACTIVE_CALLS:
+            try:
+                self.group_call.on_network_status_changed(self.on_network_changed)
+                self.group_call.on_playout_ended(self.playout_ended_handler)
+                await self.group_call.join(self._chat)
+            except GroupCallNotFoundError as er:
+                LOGS.info(er)
+                dn, err = await self.make_vc_active()
+                if err:
+                    return False, err
+            except Exception as e:
+                LOGS.exception(e)
+                return False, e
+        return True, None
+
+    async def on_network_changed(self, call, is_connected):
+        chat = self._chat
+        if is_connected:
+            if chat not in ACTIVE_CALLS:
+                ACTIVE_CALLS.append(chat)
+        elif chat in ACTIVE_CALLS:
+            ACTIVE_CALLS.remove(chat)
+
+    async def playout_ended_handler(self, call, source, mtype):
+        if os.path.exists(source):
+            os.remove(source)
+        await self.play_from_queue()
+
+    async def play_from_queue(self):
+        chat_id = self._chat
+        if chat_id in VIDEO_ON:
+            await self.group_call.stop_video()
+            VIDEO_ON.pop(chat_id)
+        try:
+            song, title, link, thumb, from_user, pos, dur = await get_from_queue(
+                chat_id
+            )
+            try:
+                await self.group_call.start_audio(song)
+            except ParticipantJoinMissingError:
+                await self.vc_joiner()
+                await self.group_call.start_audio(song)
+            if MSGID_CACHE.get(chat_id):
+                await MSGID_CACHE[chat_id].delete()
+                del MSGID_CACHE[chat_id]
+            text = "<strong>🎧 Now playing #{}: <a href={}>{}</a>\n⏰ Duration:</strong> <code>{}</code>\n👤 <strong>Requested by:</strong> {}".format(
+                pos, link, title, dur, from_user
+            )
+            try:
+                xx = await vcClient.send_message(
+                    self._current_chat,
+                    "<strong>🎧 Now playing #{}: <a href={}>{}</a>\n⏰ Duration:</strong> <code>{}</code>\n👤 <strong>Requested by:</strong> {}".format(
+                        pos, link, title, dur, from_user
+                    ),
+                    file=thumb,
+                    link_preview=False,
+                    parse_mode="html",
+                )
+            except ChatSendMediaForbiddenError:
+                xx = await vcClient.send_message(
+                    self._current_chat, text, link_preview=False, parse_mode="html"
+                )
+            MSGID_CACHE.update({chat_id: xx})
+            VC_QUEUE[chat_id].pop(pos)
+            if not VC_QUEUE[chat_id]:
+                VC_QUEUE.pop(chat_id)
+
+        except (IndexError, KeyError):
+            await self.group_call.stop()
+            del CLIENTS[self._chat]
+            await vcClient.send_message(
+                self._current_chat,
+                f"• Successfully Left Vc : <code>{chat_id}</code> •",
+                parse_mode="html",
+            )
+        except Exception as er:
+            LOGS.exception(er)
+            await vcClient.send_message(
+                self._current_chat,
+                f"<strong>ERROR:</strong> <code>{format_exc()}</code>",
+                parse_mode="html",
+            )
