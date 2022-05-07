21d28cc9d57d368830d4840b76f370302e16cd69
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-05-07 13:58:18 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -171,7 +171,7 @@ async def amireallyalive(alive):
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
         f"**╭─━━━━━━━━━━━─╮** \n"
-        f"**             ⚡ 𝗗 𝗭 𝗟​ ⚡  ** \n"
+        f"**            ⚡ 𝗗 𝗭 𝗟​ ⚡  ** \n"
         f"**╰─━━━━━━━━━━━─╯** \n\n"
         f"**★ Bot Is Running Up...★ ** \n\n"
         f"{emoji} **x_x :** [{user.first_name}](tg://user?id={user.id}) \n"
